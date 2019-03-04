82445a67efa06a6532c94b895854f4cb6117079f
Raíssa Faccioli <raissafaccioli@outlook.com>
2019-03-04 13:33:30 -0300

    add php files instead of html

---
+++ prato.php
@@ -0,0 +1,36 @@
+
+	<?php include 'header.php'; ?>
+
+  <div class="product-page small-11 large-12 columns no-padding small-centered">
+
+		<div class="global-page-container">
+
+			<div class="product-section">
+				<div class="product-info small-12 large-5 columns no-padding">
+					<h3>Camarão ao Alho</h3>
+					<h4>Entradas</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales
+						justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod
+						efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit
+						sit amet, iaculis porta velit. In vulputate tristique urna. Praesent
+						tempus ipsum augue, sit amet tristique lacus semper cursus.
+					</p>
+
+					<h5><b>Preço: </b>R$ 14</h5>
+					<h5><b>Calorias: </b>310</h5>
+				</div>
+
+				<div class="product-picture small-12 large-7 columns no-padding">
+					<img src="img/cardapio/camarao-alho.jpg" alt="camarao">
+				</div>
+
+			</div>
+
+			<div class="go-back small-12 columns no-padding">
+				<a href="cardapio.html">
+					<< Voltar ao Cardápio</a> </div> </div> </div> <footer id="footer" class="small-12 columns no-padding">
+
+        <?php include 'footer.php'; ?>
+</body>
+
+</html>
